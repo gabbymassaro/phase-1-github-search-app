42f77f5de0b4362a834f35affa3edf1cc2b8950d
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-23 10:56:44 -0400

    Repo list seems to be working, API timeout

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-// required to add custom header to your requests:
-// Accept: application/vnd.github.v3+json
-
-// User Search Endpoint: You can search for users matching a certain name
-// Example: if we wanted to find all users named octocat, we would make 
-// a GET request to https://api.github.com/search/users?q=octocat. 
-// To view the response, you can copy and paste that URL into your browser. ✔️
-
 // User Repos Endpoint: You can find all the public repositories 
 // for a user using this endpoint. 
 // Example: if we wanted to find all the repositories for a user with GitHub 
@@ -14,23 +6,16 @@
 // copy and paste that URL into your browser. 
 
 // Deliverables:
-// 1. The index.html file has a form with a search input. When the form is submitted, 
-// it should take the value of the input and search GitHub for user matches using the 
-// User Search Endpoint. ✔️
-//
-// 2. Using the results of the search, display information about the users to the page.
-// (You might include showing their username, avatar and a link to their profile.) ✔️
-//
-// 3. Clicking on one of these users should send a request to the User Repos Endpoint
-// and return data about all the repositories for that user.
-//
 // 4. Using the response from the Users Repos Endpoint, display all the repositories 
 // for that user on the page.
+
+
 document.addEventListener("DOMContentLoaded", () => {
   
   let form = document.getElementById("github-form") 
   let gitHubContainer = document.getElementById('github-container')
   let userList = document.getElementById('user-list') 
+  let repoList = document.getElementById('repos-list')
 
   form.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -45,7 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(response => response.json())
     .then(data => {
       data.items.forEach(createUsers);
-      console.log(data.items);
     })
   })
 
@@ -58,11 +42,25 @@ document.addEventListener("DOMContentLoaded", () => {
     img.src = user.avatar_url
     userList.appendChild(img)
 
+    img.addEventListener("click", () => {      
+      fetch(`https://api.github.com/users/${user.login}/repos`, {
+        headers: {
+          "Accept": "application/vnd.github.v3+json"
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        const ol = document.createElement('ol')
+        ol.textContent = data.name
+        repoList.appendChild(ol)
+      })
+    })
+
     const p = document.createElement('p')
     p.textContent = user.html_url
     userList.appendChild(p)
   }
-  
 })
 
 
